37dbe5059573afb38e7cd7275eaa02ebe8bd66d3
triophore <venusdharan@gmail.com>
2025-04-13 23:40:16 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LuckFoxStarter
 LuckFoxStarter repo
+
+
+```sh
+    git clone --recurse-submodules git@github.com:Triophore/LuckFoxStarter.git
+```
--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@
 
 echo installing dependencies for ubuntu
 
-sudo apt-get install -y git ssh make gcc gcc-multilib g++-multilib module-assistant expect g++ gawk texinfo libssl-dev bison flex fakeroot cmake unzip gperf autoconf device-tree-compiler libncurses5-dev pkg-config bc python-is-python3 passwd openssl openssh-server openssh-client vim file cpio rsync screen git
-
-git pull "$(pwd)/SDK/luckfox-pico/"
+sudo apt-get install -y ssh make gcc gcc-multilib g++-multilib module-assistant expect g++ gawk texinfo libssl-dev bison flex fakeroot cmake unzip gperf autoconf device-tree-compiler libncurses5-dev pkg-config bc python-is-python3 passwd openssl openssh-server openssh-client vim file cpio rsync screen git
